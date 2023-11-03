bdb2d649b1e135d2facd80830213de81f11dcbc6
Hasham Ahmad <145142819+HashamAhmad09@users.noreply.github.com>
2023-11-03 11:12:57 +0500

    Update Tokenization.java

--- Tokenization.java
+++ Tokenization.java
@@ -1,8 +1,16 @@
-
 public class Tokenization {
 
-	public Tokenization() {
-		// TODO Auto-generated constructor stub
+	private void tokenize(String verse){
+	String[] tokens = verse.split("[\\s.,!?]+");
+
+
+
+		for (int i = 0; i < tokens.length; ++i) {
+
+			tokensList.addItem(tokens[i]);
+
+		}
+
 	}
 
 }
